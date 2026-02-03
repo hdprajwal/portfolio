be329aece01b377051714ce17ac773b126d4ae61
HD Prajwal <hdprajwal01@gmail.com>
2026-02-03 11:35:43 -0500

    feat: adds sooner for toast

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,17 +1,19 @@
 import Footer from "@/components/Footer";
 import SiteHeader from "@/components/SiteHeader";
 import CommandMenu from "@/components/CommandMenu";
+import { Toaster } from "@/components/ui/sooner"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <div className="flex flex-col items-center justify-start min-h-screen px-2">
-        <div className="w-full max-w-4xl grid-stack mt-0 border-t border-border flex flex-col min-h-screen">
+      <div className="flex flex-col items-center justify-start min-h-screen">
+        <div className="w-full max-w-3xl grid-stack mt-0 flex flex-col min-h-screen">
           <SiteHeader />
-          {children}
+          <main className="flex-1">{children}</main>
           <Footer />
         </div>
       </div>
+      <Toaster />
       <CommandMenu />
     </>
   );
